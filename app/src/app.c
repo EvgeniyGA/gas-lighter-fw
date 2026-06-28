25d1ac6c0acd7158f1fe2665383818e8ec15a8c7
EvgeniyGA <93276404+EvgeniyGA@users.noreply.github.com>
2026-06-28 19:44:47 +0300

    fix freertos with cmake

--- app/src/app.c
+++ app/src/app.c
@@ -1,28 +1,34 @@
 #include <FreeRTOS.h>
-#include <FreeRTOSConfig.h>
 #include "task.h"
+#include "SEGGER_RTT.h"
+
+#define dbg(s)                        SEGGER_RTT_WriteString( 0, s )//; HAL_Delay(1)
+#define dbgln(s)                      dbg( s "\n" )
+#define dbgf( format, ... )           SEGGER_RTT_printf( 0, ( const char * ) ( format ), ##__VA_ARGS__ ); HAL_Delay(1)
 
 void BlinkTask(void *argument)
 {
     for(;;)
     {
-    	//dbgln("from task");
-        //HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_7); // Мигаем светодиодом (замени на свой пин)
+        //HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_7);
         vTaskDelay(pdMS_TO_TICKS(500));
+        dbgln("blink from freertos!!!!");
     }
 }
 
 void init(void){
+	SEGGER_RTT_ConfigUpBuffer( 0, NULL, NULL, 0, SEGGER_RTT_MODE_NO_BLOCK_TRIM );
+    SEGGER_RTT_WriteString( 0, "SEGGER Real-Time-Terminal Sample\n" );
 }
 
 void setup(void){
 	  xTaskCreate(
-	      BlinkTask,        // Функция задачи
-	      "Blink",          // Имя (для отладки, ни на что не влияет)
-	      128,              // Размер стека в словах (128 * 4 = 512 байт)
-	      NULL,             // Параметр, передаваемый в задачу (тут не нужен)
-		  configMAX_PRIORITIES-1,                // Приоритет (1 — низкий, configMAX_PRIORITIES-1 — высокий)
-	      NULL              // Handle задачи (если хочешь управлять ею позже)
+	      BlinkTask,
+	      "Blink",
+	      128,
+	      NULL,
+		  configMAX_PRIORITIES-1,
+	      NULL
 	  );
 	  vTaskStartScheduler();
 }
