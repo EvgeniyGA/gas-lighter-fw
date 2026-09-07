f8bd09c52284bebe1750a4f73b98091bb4b87206
EvgeniyGA <93276404+EvgeniyGA@users.noreply.github.com>
2026-09-07 20:29:21 +0300

    Update config_service.c

--- app/services/config_service/src/config_service.c
+++ app/services/config_service/src/config_service.c
@@ -5,7 +5,7 @@
 #include <string.h> 
 #include "fatfs.h"
 
-#define STORAGE_TASK_STACK_SIZE         (configMINIMAL_STACK_SIZE * 1)
+#define STORAGE_TASK_STACK_SIZE         (configMINIMAL_STACK_SIZE * 2)
 #define STORAGE_TASK_PRIORITY           (configMAX_PRIORITIES - 5)
 
 #define STORAGE_QUEUE_LEN               (1)
@@ -22,6 +22,8 @@ static uint8_t massive_for_saving[STORAGE_MAX_SAVING_SIZE];
 typedef enum{
     STORAGE_MSG_SAVE_INT = 0,
     STORAGE_MSG_LOAD_INT,
+    STORAGE_MSG_SAVE_MAS,
+    STORAGE_MSG_LOAD_MAS,
     STORAGE_MSG_MAX_OPT
 }storage_service_msg_e;
 
@@ -30,14 +32,27 @@ typedef struct{
     uint8_t type;
     uint8_t* data;
     uint16_t datalen;
+    uint8_t result;
 }storage_service_msg_tx_t;
 
 typedef struct{
+    char* name;
     uint8_t type;
     uint8_t* data;
     uint16_t datalen;
     SemaphoreHandle_t done;
+    uint8_t result;
 }storage_service_msg_rx_t;
+
+uint8_t config_save_mas(char* name, uint8_t* data){
+    storage_service_msg_tx_t msg;
+    msg.name = name;
+    msg.type = STORAGE_MSG_SAVE_MAS;
+    msg.data = massive_for_saving;
+    msg.datalen = sizeof(data);
+    msg.result = -1;
+    return 0;
+}
 
 uint8_t config_save_int(char* name, uint32_t data){
     storage_service_msg_tx_t msg;
@@ -45,21 +60,23 @@ uint8_t config_save_int(char* name, uint32_t data){
     msg.type = STORAGE_MSG_SAVE_INT;
     msg.data = massive_for_saving;
     msg.datalen = sizeof(data);
+    msg.result = -1;
     memcpy(msg.data, &data, msg.datalen);
 
     if(xQueueSendToBack(storage_tx_queue_handle, &msg, portMAX_DELAY) != pdPASS){
-        //todo error
+        return - 1;
     }
     return 0;
 }
 
 uint8_t config_load_int(char* name, uint32_t* data){
-//    *data = 123;
     storage_service_msg_rx_t msg;
+    msg.name = name;
     msg.type = STORAGE_MSG_LOAD_INT;
     msg.data = (uint8_t*)data;
     msg.datalen = sizeof(*data);
     msg.done = xSemaphoreCreateBinary();
+    msg.result = -1;
     if(xQueueSendToBack(storage_rx_queue_handle, &msg, portMAX_DELAY) != pdPASS){
         vSemaphoreDelete(msg.done);
         return -1;
@@ -80,15 +97,14 @@ SemaphoreHandle_t storage_get_fs_mutex(void){
     return fs_mutex;
 }
 
+static FIL fil;
+static char buf[10];
+
 void config_service_tx_task(void* param){
     static uint8_t storage_queue_buffer[STORAGE_QUEUE_LEN * sizeof(storage_service_msg_tx_t)];
     storage_service_msg_tx_t msg;
     static FRESULT res;
-    static DIR dir;
-    static FILINFO fno;
-    static FIL fil;
     static uint32_t byteswritten, len;
-    static char wdata[32];
     storage_tx_queue_handle = xQueueCreateStatic(STORAGE_QUEUE_LEN, sizeof(storage_service_msg_tx_t), storage_queue_buffer, &storage_rx_queue_def);
     while(1){
         if(xQueueReceive(storage_tx_queue_handle, &msg, portMAX_DELAY)){
@@ -96,29 +112,19 @@ void config_service_tx_task(void* param){
             if(msg.type == STORAGE_MSG_SAVE_INT){
                 int tmp;
                 memcpy(&tmp, msg.data, msg.datalen);
-                printf("storage will save %d\n\r", tmp);
 
-                if(f_opendir(&dir, "/") == 0){
-                    f_readdir(&dir, &fno);
-                    if(f_open(&fil, msg.name, FA_CREATE_ALWAYS | FA_WRITE) != FR_OK){
-
+                if(f_open(&fil, msg.name, FA_CREATE_ALWAYS | FA_WRITE) == FR_OK){
+                    len = snprintf(buf, sizeof(buf), "%d", tmp);//todo
+                    buf[len] = 0;
+                    res = f_write(&fil, buf, sizeof(buf), (void*)&byteswritten);
+                    if((byteswritten != 0) && (res == FR_OK)){
+                        msg.result = 0;
                     }
-                    else{
-                        len = snprintf(wdata, sizeof(wdata), "%d", tmp);//todo
-                        wdata[len] = 0;
-                        res = f_write(&fil, wdata, sizeof(wdata), (void*)&byteswritten);
-                        if((byteswritten == 0) || (res != FR_OK)){
-                            printf("write error\n\r");
-                        }
-                        else{
-                            printf("written string %s\n\r", wdata);
-                        }
-                        f_close(&fil);
-                    }
+                    f_close(&fil);
                 }
             }
             else{
-
+//todo
             }
             xSemaphoreGive(fs_mutex);
         }
@@ -129,35 +135,28 @@ void config_service_rx_task(void* param){
     static uint8_t storage_queue_buffer[STORAGE_QUEUE_LEN * sizeof(storage_service_msg_rx_t)];
     storage_service_msg_rx_t msg;
     storage_rx_queue_handle = xQueueCreateStatic(STORAGE_QUEUE_LEN, sizeof(storage_service_msg_rx_t), storage_queue_buffer, &storage_tx_queue_def);
+    static UINT br;
+    
     while(1){
         if(xQueueReceive(storage_rx_queue_handle, &msg, portMAX_DELAY)){
             xSemaphoreTake(fs_mutex, portMAX_DELAY);
 
             if(msg.type == STORAGE_MSG_LOAD_INT){
-                /*
-                    FIL file;
-                    char buf[32];
-                    UINT br;
-
-                    if(f_open(&file, msg.filename, FA_READ) == FR_OK){
-                        f_read(&file, buf, sizeof(buf) - 1, &br)
-                        f_close(&file);
-                        buf[br] = '\0';
-                        if(msg.type == STORAGE_MSG_LOAD_INT){
-                            msg.out = atoi(buf);
-                        }
+                uint32_t loaded_val = 0;
+                if(f_open(&file, msg.name, FA_READ) == FR_OK){
+                    f_read(&file, buf, sizeof(buf) - 1, &br);
+                    f_close(&file);
+                    buf[br] = '\0';
+                    if(msg.type == STORAGE_MSG_LOAD_INT){
+                        loaded_val = atoi(buf);
+                        msg.result = 0;
                     }
-                    else{
-                        //msg.resutl
-                    }
-                */
-                uint32_t test_val = 777;
-                memcpy(msg.data, &test_val, msg.datalen);
+                }
+                memcpy(msg.data, &loaded_val, sizeof(loaded_val));
             }
 
             xSemaphoreGive(fs_mutex);
             xSemaphoreGive(msg.done);
-
         }
     }
 }
