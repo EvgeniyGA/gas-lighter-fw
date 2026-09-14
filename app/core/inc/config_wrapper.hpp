aa567edc75857eb0ed4251b38e84fae5ad2f3e15
EvgeniyGA <93276404+EvgeniyGA@users.noreply.github.com>
2026-09-14 23:02:32 +0300

    Update config_wrapper.hpp

--- app/core/inc/config_wrapper.hpp
+++ app/core/inc/config_wrapper.hpp
@@ -5,55 +5,54 @@
 #include "config_service.h"
 
 template <typename T>
-struct ConfigTypeInfo{
-    using base_type = T;
-    static constexpr size_t element_count = 1;
-    static constexpr size_t element_size = sizeof(T);
+struct TypeInfo{
+    using BaseType = T;
+    static constexpr size_t ElementSize = sizeof(T);
+    static constexpr size_t ElementCount = 1;
 };
 
 template <typename T, size_t N>
-struct ConfigTypeInfo<std::array<T, N>>{
-    using base_type = T;
-    static constexpr size_t element_count = N;
-    static constexpr size_t element_size = sizeof(T);
+struct TypeInfo<std::array<T, N>>{
+    using BaseType = T;
+    static constexpr size_t ElementSize = sizeof(T);
+    static constexpr size_t ElementCount = N;
 };
-
-
-
 
 template <typename T>
 struct ConfigItem{
-    const char* param_name;
-    const T default_value;
-    T current_value;
+    const char* name;
+    const T def_value;
+    T value;
+    ConfigItem(const char* param_name, T default_value) : 
+        name(param_name), def_value(default_value){};
 
-    constexpr ConfigItem(const char* name, T def_val) 
-        : param_name(name), default_value(def_val), current_value{} {};
-
-    void load(){
-        static_assert(std::is_trivially_copyable_v<T>, "wrong type");//???
-        T value = default_value;
-
-        using TypeInfo = ConfigTypeInfo<T>;
-
-        config_data_type_e type = CONFIG_DATA_TYPE_INT;
-        if constexpr (std::is_same_v<typename TypeInfo::base_type, float>){
-            type = CONFIG_DATA_TYPE_FLOAT;
-        }
-        
-        config_load_raw(
+    T load(){
+        T value = def_value;
+        config_data_type_e type = std::is_same_v<typename TypeInfo<T>::BaseType, float> ? CONFIG_DATA_TYPE_FLOAT : CONFIG_DATA_TYPE_INT;
+        uint8_t res = config_load_raw(
             name,
             reinterpret_cast<uint8_t*>(&value), 
-            TypeInfo::element_size, 
-            TypeInfo::element_count,
+            TypeInfo<T>::ElementSize, 
+            TypeInfo<T>::ElementCount,
             type
         );
-
+        if(res != 0){
+            save(def_value);
+        }
         return value;
-
     }
 
-
+    void save(const T val){
+        config_data_type_e type = std::is_same_v<typename TypeInfo<T>::BaseType, float> ? CONFIG_DATA_TYPE_FLOAT : CONFIG_DATA_TYPE_INT;
+        value = val;
+        config_save_raw(
+            name,
+            reinterpret_cast<uint8_t*>(&value),
+            TypeInfo<T>::ElementSize,
+            TypeInfo<T>::ElementCount,
+            type
+        );
+    }
 };
 
 struct DeviceConfig{
