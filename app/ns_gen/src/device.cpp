528fa699e32b7d8bef302f00734a84c8b5d7ec7b
EvgeniyGA <93276404+EvgeniyGA@users.noreply.github.com>
2026-09-22 21:11:57 +0300

    Update device.cpp

--- app/ns_gen/src/device.cpp
+++ app/ns_gen/src/device.cpp
@@ -22,27 +22,44 @@ TimerHandle_t fire_away_timer, xLedOffTimer;
 void heartbit_callback(void){
 }
 
-void start_generation(void){
+uint8_t is_started = 0;
+
+void update_period(){
   auto period = config.getConfig(config_step).value[0];
   if(period != 0){
     xTimerChangePeriod(fire_away_timer, pdMS_TO_TICKS(period), 0);
   }
   else{
-    //todo
+    xTimerChangePeriod(fire_away_timer, 1, 0);
   }
-  xTimerStart(fire_away_timer, 0);
+}
+
+void start_generation(void){
+  auto period = config.getConfig(config_step).value[0];
+  if((period != 0) && (is_started == 0)){
+    is_started = 1;
+  }
+  else{
+    is_started = 0;
+  }
+  if(is_started || (period == 0)){
+    xTimerReset(fire_away_timer, 0);
+  }
 }
 
 void vRunTimerCallback( TimerHandle_t xTimer ) {
-  gpio_led_go_change_state(GPIO_LED_ON);
-  if(config.getConfig(config_step).value[0] == 0){
-    xTimerStop(fire_away_timer, 0);
+  auto period = config.getConfig(config_step).value[0];
+  if(is_started || (period == 0)){
+    gpio_led_go_change_state(GPIO_LED_ON);
+    if(period != 0){
+      xTimerReset(fire_away_timer, 0);
+    }
+    dma_send_data_to_fsmc(
+      reinterpret_cast<int*>(config.getConfig(config_step).value.data() + 1), 
+      config.getConfig(config_step).value.size() - 1
+    );
+    xTimerReset(xLedOffTimer, portMAX_DELAY);
   }
-  dma_send_data_to_fsmc(
-    reinterpret_cast<int*>(config.getConfig(config_step).value.data() + 1), 
-    config.getConfig(config_step).value.size() - 1
-  );
-  gpio_led_go_change_state(GPIO_LED_OFF);
 }
 
 void refresh_task(void* param){
@@ -53,20 +70,20 @@ void refresh_task(void* param){
       gpio_led_mode_change_state(config_step, GPIO_LED_OFF);
       config_step = (config_step < config_steps) ? (config_step + 1) : 0;
       gpio_led_mode_change_state(config_step, GPIO_LED_ON);
-      xTimerStop(fire_away_timer, 0);
+      update_period();
     }
 
-    do{
+    while(is_button_mode_pressed()){
       vTaskDelay(100 / portTICK_PERIOD_MS);
     }
-    while(is_button_mode_pressed());
+    
     gpio_led_status_change_state(GPIO_LED_OFF);
 
     if(is_button_go_pressed() ){
       start_generation(); 
-      do{
+      while(is_button_go_pressed()){
         vTaskDelay(100 / portTICK_PERIOD_MS);
-      }while(is_button_go_pressed());
+      }
     }
 
     vTaskDelay(10 / portTICK_PERIOD_MS);
@@ -79,13 +96,11 @@ StaticTask_t refresh_task_taskdef;
 
 void init(void){
   static StackType_t refresh_task_stack[refresh_task_stack_size];
-  fire_away_timer = xTimerCreate("go_timer", pdMS_TO_TICKS(100), pdTRUE, (void*)0, vRunTimerCallback);
+  fire_away_timer = xTimerCreate("go_timer", pdMS_TO_TICKS(100), pdFALSE, (void*)0, vRunTimerCallback);
   xLedOffTimer = xTimerCreate("led_off_timer", pdMS_TO_TICKS(200), pdFALSE, (void*)0,
-    [](TimerHandle_t xTimer){gpio_led_go_change_state(GPIO_LED_OFF);}
+    [](TimerHandle_t xTimer){ gpio_led_go_change_state(GPIO_LED_OFF); }
   );
-  xTimerStart(xLedOffTimer, portMAX_DELAY);
- // xTimerStart(fire_away_timer, 0);
-  
+  update_period();
   xTaskCreateStatic(refresh_task, "refresh_task", refresh_task_stack_size, 
     NULL, configMAX_PRIORITIES - 3, refresh_task_stack, &refresh_task_taskdef);
 }
